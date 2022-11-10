b2b4a4777e33e9e666766807776bdf2602c37083
“oy” <2542933857@qq.com>
2022-11-10 13:52:52 +0800

    sort_1

---
+++ EX_10_17/User/Inc/sort.h
@@ -0,0 +1,13 @@
+//
+// Created by 86187 on 2022/10/17.
+//
+
+#ifndef LEARNING_SORT_H
+#define LEARNING_SORT_H
+
+#include "node.h"
+
+void InsertSort(Node *p);
+
+
+#endif //LEARNING_SORT_H
--- EX_10_17/main.c
+++ EX_10_17/main.c
@@ -2,6 +2,7 @@
 #include "User/Inc/node.h"
 #include "User/Inc/seqlist.h"
 #include "User/Inc/tree.h"
+#include "sort.h"
 
 void OddEven(int A[], int len) {
     int B[len];
@@ -120,94 +121,100 @@ int main() {
 
 
     /*二叉树*/
-    TreeRoot *t;
-    int max=0;
-    int x=0;
-    int n;
-    t=TreeRootInit();
-    char *s;
-    s=(char *)malloc(20*sizeof(char));
-    printf("\t\t\t\t\t\t实验三：二叉树\n");
-    printf("*********************请输入前序二叉树以 # 为断点********************\n");
-    printf("功能列表：\n");
-    printf("1.输入前序，构建二叉树\n");
-    printf("2.前序打印\n");
-    printf("3.中序打印\n");
-    printf("4.后序打印\n");
-    printf("5.层次遍历打印\n");
-    printf("6.结点总数打印\n");
-    printf("7.二叉树深度打印\n");
-    printf("8.输出度为1结点个数打印\n");
-    printf("****************************************************************\n");
-    while (1) {
-        max=0;
-        printf("功能选择：");
-        fflush(stdout);
-        scanf("%d", &n);
-        getchar();
-        fflush(stdin);
-        //printf("\n");
-        switch (n) {
-            case 1:
-                printf("请输入前序：");
-                fflush(stdout);
-                scanf("%s", s);
-                getchar();
-                fflush(stdin);
-                CreateTree(&t->root, &s);
-
-                printf("构建完毕\n");
-                printf("\n");
-                break;
-
-            case 2:
-                printf("前序序列：");
-                PrePrint(t->root);
-                printf("\n\n");
-                break;
-
-            case 3:
-                printf("中序序列：");
-                MidPrint(t->root);
-                printf("\n\n");
-                break;
-
-            case 4:
-                printf("后序序列：");
-                PostPrint(t->root);
-                printf("\n\n");
-                break;
-
-            case 5:
-                printf("层次遍历序列：");
-                LevelPrint(t->root);
-                printf("\n\n");
-                break;
-
-            case 6:
-                printf("结点总数：");
-                printf("%d\n\n", PointSum(t->root, 0));
-                break;
-
-            case 7:
-                printf("二叉树深度：");
-                DepthSum(t->root, 0, &max);
-                printf("%d\n\n", max);
-                break;
-
-            case 8:
-                printf("输出度为1结点个数：");
-                SinglePointSum(t->root, &x);
-                printf("%d\n\n", x);
-                break;
-
-            default:
-                printf("输入无效，请重新输入\n\n");
-                break;
-
-        }
-    }
+//    TreeRoot *t;
+//    int max=0;
+//    int x=0;
+//    int n;
+//    t=TreeRootInit();
+//    char *s;
+//    s=(char *)malloc(20*sizeof(char));
+//    printf("\t\t\t\t\t\t实验三：二叉树\n");
+//    printf("*********************请输入前序二叉树以 # 为断点********************\n");
+//    printf("功能列表：\n");
+//    printf("1.输入前序，构建二叉树\n");
+//    printf("2.前序打印\n");
+//    printf("3.中序打印\n");
+//    printf("4.后序打印\n");
+//    printf("5.层次遍历打印\n");
+//    printf("6.结点总数打印\n");
+//    printf("7.二叉树深度打印\n");
+//    printf("8.输出度为1结点个数打印\n");
+//    printf("****************************************************************\n");
+//    while (1) {
+//        max=0;
+//        printf("功能选择：");
+//        fflush(stdout);
+//        scanf("%d", &n);
+//        getchar();
+//        fflush(stdin);
+//        //printf("\n");
+//        switch (n) {
+//            case 1:
+//                printf("请输入前序：");
+//                fflush(stdout);
+//                scanf("%s", s);
+//                getchar();
+//                fflush(stdin);
+//                CreateTree(&t->root, &s);
+//
+//                printf("构建完毕\n");
+//                printf("\n");
+//                break;
+//
+//            case 2:
+//                printf("前序序列：");
+//                PrePrint(t->root);
+//                printf("\n\n");
+//                break;
+//
+//            case 3:
+//                printf("中序序列：");
+//                MidPrint(t->root);
+//                printf("\n\n");
+//                break;
+//
+//            case 4:
+//                printf("后序序列：");
+//                PostPrint(t->root);
+//                printf("\n\n");
+//                break;
+//
+//            case 5:
+//                printf("层次遍历序列：");
+//                LevelPrint(t->root);
+//                printf("\n\n");
+//                break;
+//
+//            case 6:
+//                printf("结点总数：");
+//                printf("%d\n\n", PointSum(t->root, 0));
+//                break;
+//
+//            case 7:
+//                printf("二叉树深度：");
+//                DepthSum(t->root, 0, &max);
+//                printf("%d\n\n", max);
+//                break;
+//
+//            case 8:
+//                printf("输出度为1结点个数：");
+//                SinglePointSum(t->root, &x);
+//                printf("%d\n\n", x);
+//                break;
+//
+//            default:
+//                printf("输入无效，请重新输入\n\n");
+//                break;
+//
+//        }
+//    }
 //test
 
+
+    /*插入排序*/
+    Node *p;
+    int x[10] = {51, 38, 79, 22, 91, 105, 33, 52, 16, 112};
+    p = CreateNode(x, 10);
+    InsertSort(p);
 
 }
